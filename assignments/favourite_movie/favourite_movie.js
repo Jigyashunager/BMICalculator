e3c6fba0d2fe10a319e30653d6aeda9ba7282987
Jigyashunager <jigyashunager@gmail.com>
2021-11-25 19:42:38 +0530

    completed

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,15 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if (operation === "add") {
+        movies.push(movie)
+    }
+    if (operation === "remove") {
+        if (movies.length > 0){
+            movies.pop()
+        }
+    }
+    return movies
 }
 
 module.exports = favouriteMovie;
